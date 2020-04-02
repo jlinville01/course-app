e37d6b6cb4c206ef5d62729082f2b874fad36ee3
Justin Linville <justinlinville@Justins-iMac.local>
2020-04-02 11:31:59 -0700

    adds destroy_all to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Course.destroy_all
 Course.create!(department: 'CS', number: '83R', title: 'Server-Side Ruby Web Programming', size: 45, hours: 3, teacher: 'Professor Stahl')
 Course.create!(department: 'ANTHRO', number: '2', title: 'Cultural Anthropology', size: 45, hours: 5, teacher: 'Professor Denman')
 Course.create!(department: 'HIST', number: '1', title: 'History of Western Civilization I', size: 30, hours: 4, teacher: 'Professor Thomasson')
